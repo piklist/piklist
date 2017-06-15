cbb20411ad587658e7347d52b59a752f406b6223
Jason Adams <jason.the.adams@gmail.com>
2017-06-15 11:26:15 -0700

    move SQL to a single line

--- includes/class-piklist-revision.php
+++ includes/class-piklist-revision.php
@@ -47,13 +47,7 @@ class Piklist_Revision
 
     if (($parent_id = wp_is_post_revision($post_id)) && !wp_is_post_autosave($post_id))
     {
-      $wpdb->get_results($wpdb->prepare("
-        INSERT INTO $wpdb->postmeta
-          (post_id, meta_key, meta_value)
-          SELECT %d, meta_key, meta_value
-            FROM $wpdb->postmeta
-            WHERE post_id = %d
-      ", $post_id, $parent_id));
+      $wpdb->get_results($wpdb->prepare("INSERT INTO $wpdb->postmeta (post_id, meta_key, meta_value) SELECT %d, meta_key, meta_value FROM $wpdb->postmeta WHERE post_id = %d", $post_id, $parent_id));
     }
   }
 
