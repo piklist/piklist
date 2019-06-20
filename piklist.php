299cf173ee66bf5a6778c2137e9251d8022c08f0
Steven Bruner <sbruner@slipfire.com>
2019-06-20 13:10:55 -0400

    Piklist 1.0.4

--- piklist.php
+++ piklist.php
@@ -3,7 +3,7 @@
 Plugin Name: Piklist
 Plugin URI: https://piklist.com
 Description: The most powerful framework available for WordPress.
-Version: 1.0.3
+Version: 1.0.4
 Author: Piklist
 Author URI: https://piklist.com
 Text Domain: piklist
@@ -12,7 +12,7 @@ License: GPLv2
 */
 
 /*
-  Copyright (c) 2012-2018 Piklist, LLC.
+  Copyright (c) 2012-2019 Piklist, LLC.
   All rights reserved.
 
   This software is distributed under the GNU General Public License, Version 2,
