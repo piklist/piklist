fa3663377610e86b8b0adec336f3f13944786771
Steve Bruner <stevenbruner@Steves-MacBook-Pro.local>
2020-04-23 22:01:26 -0400

    1.07

--- piklist.php
+++ piklist.php
@@ -3,7 +3,7 @@
 Plugin Name: Piklist
 Plugin URI: https://piklist.com
 Description: The most powerful framework available for WordPress.
-Version: 1.0.6
+Version: 1.0.7
 Author: Piklist
 Author URI: https://piklist.com
 Text Domain: piklist
